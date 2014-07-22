da6bcb5313f6b91eeda7430659bf9184385b6227
vnicula <vnicula@users.noreply.github.com>
2014-07-22 10:24:32 -0400

    Create node.h

---
+++ plugin/node.h
@@ -0,0 +1,10 @@
+
+class node
+{
+public:
+  static void configure( options const& opt );
+  static void run_all( parameters const& par );
+  static void command( string &cmd );
+  static bool is_alive( );
+  
+}; //class node
